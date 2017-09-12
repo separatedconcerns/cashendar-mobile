b4a5e98fc9800c2cf2add7248a4d9cf546822bd8
shaikathaque <shaikathaque4@gmail.com>
2017-09-12 13:45:31 -0700

    add map component

---
+++ src/screens/MapScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class MapScreen extends React.Component {
+  // static navigationOptions = {
+  //   title: 'Dashboard',
+  // };
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Map of your spendings</Text>
+      </View>
+    );
+  }
+}
