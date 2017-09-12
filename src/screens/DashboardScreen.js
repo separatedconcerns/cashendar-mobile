ef73a1df2816763daa3d38b04844ca506eeef9d4
shaikathaque <shaikathaque4@gmail.com>
2017-09-12 15:17:12 -0700

    retrieve daily spending from server and render

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { TabNavigator } from 'react-navigation';
 import { StyleSheet, Text, View } from 'react-native';
-import MapScreen from './MapScreen';
-import SettingsScreen from './SettingsScreen';
-
+import axios from 'axios';
+import qs from 'qs';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,26 +12,37 @@ const styles = StyleSheet.create({
   },
 });
 
-// class DashboardScreen extends React.Component {
-//   static navigationOptions = {
-//     title: 'Dashboard',
-//   };
-//   constructor() {
-//     super();
-//     this.state = {
-//       user: null,
-//     };
-//   }
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Your Daily Spending</Text>
-//       </View>
-//     );
-//   }
-// }
+export default class DashboardScreen extends React.Component {
 
-export default TabNavigator({
-  Map: { screen: MapScreen },
-  Settings: { screen: SettingsScreen },
-});
+  constructor() {
+    super();
+    this.state = {
+      user: '2T5SetDIL2OJJ8AZtDACuuVUrGz1',
+      dailySpending: null,
+    };
+  }
+
+  componentWillMount() {
+    this.getDailySpending();
+  }
+
+  getDailySpending() {
+    const config = {
+      url: 'http://localhost:5000/testproject-6177f/us-central1/getDailySpending',
+      payload: qs.stringify({ uniqueUserId: this.state.user }),
+    };
+    axios.post(config.url, config.payload)
+      .then((response) => {
+        this.setState({ dailySpending: response.data[Object.keys(response.data)[0]] });
+      })
+      .catch(console.error);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Your Daily Spending: { this.state.dailySpending ? this.state.dailySpending : 'N/A'}</Text>
+      </View>
+    );
+  }
+}
