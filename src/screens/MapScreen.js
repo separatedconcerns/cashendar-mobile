09691aca70dfa55e538dcf8195aeb89cbd0a8660
shaikathaque <shaikathaque4@gmail.com>
2017-09-13 16:37:52 -0700

    add more fake data

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -34,13 +34,35 @@ export default class MapScreen extends React.Component {
       >
         <MapView.Marker
           coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: 37.783668,
+            longitude: -122.432573,
+          }}
+          title={'Boba Guys'}
+          description={'September 3'}
+        >
+          <PriceMarker amount={9.00} />
+        </MapView.Marker>
+
+        <MapView.Marker
+          coordinate={{
+            latitude: 37.782999,
+            longitude: -122.418953,
           }}
           title={'KFC'}
           description={'September 14'}
         >
           <PriceMarker amount={22.14} />
+        </MapView.Marker>
+
+        <MapView.Marker
+          coordinate={{
+            latitude: 37.783697,
+            longitude: -122.408966,
+          }}
+          title={'Hack Reactor'}
+          description={'July 3'}
+        >
+          <PriceMarker amount={17880} />
         </MapView.Marker>
 
       </MapView>
