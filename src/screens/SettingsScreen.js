01fba26dcb5bedf3f633945202a9ad304d5dbb84
Kevin Labs <kevinlabx@gmail.com>
2017-09-13 21:31:16 -0500

    Update CSS on flatview on setting view

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingTop: 22
+   },
+   item: {
+     padding: 10,
+     fontSize: 18,
+     height: 44,
+   },
 });
 
 export default class SettingsScreen extends React.Component {
