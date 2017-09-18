648d0eef95add0eec66efc9a2b1d81c3fc9249d1
shaikathaque <shaikathaque4@gmail.com>
2017-09-18 16:48:22 -0700

    add state for first time user

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -3,6 +3,7 @@ import { createStore } from 'redux';
 const defaultState = {
   uniqueUserId: null,
   dailySpending: 25,
+  firstTimeUser: false,
 };
 
 function userStore(state = defaultState, action) {
@@ -10,10 +11,12 @@ function userStore(state = defaultState, action) {
     case 'LOG_IN':
       return Object.assign({}, state, {
         uniqueUserId: action.uniqueUserId,
+        firstTimeUser: action.firstTimeUser,
       });
     case 'LOG_OUT':
       return Object.assign({}, state, {
         uniqueUserId: null,
+        firstTimeUser: false,
       });
     default:
       return state;
