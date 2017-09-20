b160ffde2dbdcd4e3ed66c39692f86c69075d4a9
shaikathaque <shaikathaque4@gmail.com>
2017-09-20 13:00:13 -0700

    remove bank on switching off list item

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {AlertIOS} from 'react-native';
+import { AlertIOS } from 'react-native';
 import {
+  Body,
   Container,
   Content,
+  Icon,
   ListItem,
-  Text,
-  Body,
   Right,
   Separator,
   Switch,
-  Icon
+  Text,
 } from 'native-base';
-import {StackNavigator} from 'react-navigation';
 import axios from 'axios';
 import qs from 'qs';
 import store from '../store/userStore';
@@ -21,55 +20,57 @@ export default class SettingsScreen extends React.Component {
   constructor() {
     super();
     this.state = {
-      test: false,
-      uniqueUserId: store
-        .getState()
-        .uniqueUserId,
-
-      randomGeneratorList: [
-        'Chase',
-        'Bank of America',
-        'Wells Fargo',
-        'Citi Bank',
-        'U.S. Bank',
-        'Capital One'
-      ],
-
+      switchState: true,
+      uniqueUserId: store.getState().uniqueUserId,
       linkedAccounts: [
-        {
-          name: 'Chase',
-          tokenId: '31337'
-        },
-        {
-          name: 'Bank of America',
-          tokenId: '31337'
-        }
-      ]
+        { name: 'Chase', tokenId: '31337', active: true },
+        { name: 'Bank of America', tokenId: '31337', active: true },
+      ],
     };
-    this.deleteProfile = this
-      .deleteProfile
-      .bind(this);
-    this.logout = this
-      .logout
-      .bind(this);
+
+    this.deleteProfile = this.deleteProfile.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  handleSwitchValueChange(index) {
+    const linkedAccountsCopy = this.state.linkedAccounts.slice();
+    linkedAccountsCopy.splice(index, 1);
+    this.setState({ linkedAccounts: linkedAccountsCopy });
   }
 
   logout() {
-    store.dispatch({type: 'LOG_OUT'});
+    store.dispatch({ type: 'LOG_OUT' });
     this.navigateToApp();
   }
 
-  deleteProfileConfirm() {
-    AlertIOS.alert('Delete profile', 'Do you wish to delete the profile?', [
+  deleteInstitutionConfirmation(index) {
+    AlertIOS.alert('Unlink Bank', 'Are you sure you want to unlink this bank?', [
       {
-        text: 'Okay',
-        onPress: () => this.deleteProfile(),
-        style: 'cancel'
-      }, {
+        text: 'Yes',
+        onPress: () => this.handleSwitchValueChange(index),
+        style: 'cancel',
+      },
+      {
         text: 'Cancel',
-        style: 'cancel'
-      }
-    ],);
+        style: 'cancel',
+      },
+    ],
+   );
+  }
+
+  deleteProfileConfirm() {
+    AlertIOS.alert('Delete Profile', 'Do you wish to delete the profile?', [
+      {
+        text: 'Yes',
+        onPress: () => this.deleteProfile(),
+        style: 'cancel',
+      },
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+    ],
+   );
   }
 
   deleteProfile() {
@@ -77,7 +78,7 @@ export default class SettingsScreen extends React.Component {
     console.log(uniqueUserId);
     const config = {
       url: 'http://localhost:5000/testproject-6177f/us-central1/deleteUserProfile',
-      payload: qs.stringify({uniqueUserId})
+      payload: qs.stringify({ uniqueUserId }),
     };
     axios
       .post(config.url, config.payload)
@@ -86,9 +87,9 @@ export default class SettingsScreen extends React.Component {
           {
             text: 'Okay',
             onPress: () => this.navigateToApp(),
-            style: 'cancel'
-          }
-        ],);
+            style: 'cancel',
+          },
+        ]);
       })
       .catch(error => console.log(error));
   }
@@ -96,9 +97,7 @@ export default class SettingsScreen extends React.Component {
   // Navigation =========
   navigateToPlaid() {
     this
-      .props
-      .navigation
-      .navigate('Plaid');
+      .props.navigation.navigate('Plaid');
   }
 
   navigateToApp = () => {
@@ -118,36 +117,35 @@ export default class SettingsScreen extends React.Component {
           </Separator>
 
           {/* -======= Bank List =======- */}
-          {this
-            .state
-            .linkedAccounts
-            .map((account) => {
-              return (
-                <ListItem last key={account.name}>
-                  <Body>
-                    <Text>{account.name}</Text>
-                  </Body>
-                  <Right>
-                    <Switch />
-                  </Right>
-                </ListItem>
-              );
-            })}
+          {this.state.linkedAccounts.map((account, index) => {
+            return (
+              <ListItem last key={ index }>
+                <Body>
+                  <Text>{account.name}</Text>
+                </Body>
+                <Right>
+                  <Switch 
+                    value={this.state.linkedAccounts[index].active}
+                    onValueChange={() => this.deleteInstitutionConfirmation(index)}
+                  />
+                </Right>
+              </ListItem>
+            );
+          })}
 
           {/* =========================== */}
 
-          <Separator bordered></Separator>
+          <Separator bordered />
 
           <ListItem last>
             <Body>
-              <Text>Link new account</Text>
+              <Text>Link New Bank</Text>
             </Body>
             <Right>
               <Icon
-                onPress={this
-                .navigateToPlaid
-                .bind(this)}
-                name="add"/>
+                onPress={this.navigateToPlaid.bind(this)}
+                name="add"
+              />
             </Right>
           </ListItem>
 
@@ -159,9 +157,12 @@ export default class SettingsScreen extends React.Component {
             <Body>
               <Text
                 style={{
-                color: 'red',
-                textAlign: 'center'
-              }}>Delete Your Where's My Money Profile</Text>
+                  color: 'red',
+                  textAlign: 'center',
+                }}
+              >
+                Delete Your Where's My Money Profile
+              </Text>
             </Body>
           </ListItem>
 
@@ -169,9 +170,12 @@ export default class SettingsScreen extends React.Component {
             <Body>
               <Text
                 style={{
-                color: 'red',
-                textAlign: 'center'
-              }}>Logout</Text>
+                  color: 'red',
+                  textAlign: 'center',
+                }}
+              >
+                Logout
+              </Text>
             </Body>
           </ListItem>
 
